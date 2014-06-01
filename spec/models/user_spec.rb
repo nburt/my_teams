c4b1ef29a364372dfa5217ee993e7b63ce2af88b
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-31 23:11:30 -0600

    forgot to add user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe User do
+
+  describe 'registration' do
+
+    before do
+      @user = User.create!(:email => 'nate@example.com', :password => 'password')
+    end
+
+    it 'cannot create a user without an email' do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'cannot create a user without a password' do
+      @user.password_digest = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'cannot create a user with the same email' do
+      user = User.create(:email => 'nate@example.com', :password => 'password')
+      expect(user).to_not be_valid
+    end
+
+    it 'must create a user with a valid email' do
+      user = User.new(:email => 'nate', :password => 'password')
+      expect(user).to_not be_valid
+    end
+  end
+
+end
