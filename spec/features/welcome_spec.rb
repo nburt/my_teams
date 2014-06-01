a400a8a7a972af9b748cf748cb51624e2f27ec9f
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-31 21:30:56 -0600

    added features specs back

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'visiting the homepage' do
+
+  scenario 'displays mariners headlines on homepage' do
+    VCR.use_cassette('features/mariners_news') do
+      visit '/'
+      expect(page).to have_content 'Martinez, Cabrera lead Detroit past Seattle 6-3'
+      expect(page).to have_link 'Read on ESPN'
+    end
+  end
+
+  scenario 'displays clippers headlines on homepage' do
+    VCR.use_cassette('features/clippers_news') do
+      visit '/'
+      expect(page).to have_content 'Clips sale pending board approval'
+    end
+  end
+
+  scenario 'displays chargers headlines on homepage' do
+    VCR.use_cassette('features/chargers_news') do
+      visit '/'
+      expect(page).to have_content 'Shawne Merriman joins WWE'
+    end
+  end
+end
